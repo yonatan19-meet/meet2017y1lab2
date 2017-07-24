92fc86751505c17ca0e86335d832663584058766
yonatan19-meet <yonatan19@meet.mit.edu>
2017-07-24 16:51:30 +0300

    Stopping here

---
+++ squareinturtle.py
@@ -0,0 +1,12 @@
+side_length=67
+import turtle
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.goto(0,side_length)
+turtle.goto(side_length,side_length)
+turtle.goto(side_length,0)
+turtle.goto(0,0)
+turtle.mainloop()
+
+
